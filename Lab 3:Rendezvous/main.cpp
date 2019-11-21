c6735722f5b6c2759cd2652bbc21845d6de9465f
brendanmitchell1979 <47219784+brendanmitchell1979@users.noreply.github.com>
2019-11-21 18:14:33 +0000

    Add files via upload

---
+++ Lab 3:Rendezvous/main.cpp
@@ -0,0 +1,72 @@
+
+
+/*!
+   \file main.cpp
+   \author brendan Mitchell
+   \date 21/11/2019
+   \licence GNU
+   \brief  This is the third lab which implements a rendezvous using Semaphores .
+*/
+
+
+#include "Semaphore.h"
+#include <iostream>
+#include <thread>
+#include <chrono>
+
+/*! \class Signal
+    \brief An Implementation of a Rendezvous using Semaphores
+    Uses C++11 features such as mutex and condition variables to implement an example of a rendezvous for threads
+
+*/
+
+/*! displays a message that is split in to 2 sections to show how a rendezvous works*/
+
+/*! \fn void taskOne
+    \brief Description of void taskOne function
+    \param Semaphore firstSem
+    \param Semaphor secondSem
+    \param int delay
+    
+*/
+void taskOne(std::shared_ptr<Semaphore> firstSem,std::shared_ptr<Semaphore>  secondSem, int delay){
+  std::this_thread::sleep_for(std::chrono::seconds(delay));
+  std::cout <<"Task One has arrived! "<< std::endl;
+  //THIS IS THE RENDEZVOUS POINT!
+  firstSem->Signal();
+  secondSem->Wait();
+  std::cout << "Task One has left!"<<std::endl;
+  
+}
+
+/*! \fn void taskTwo
+    \brief Description of void taskTwo function
+    \param Semaphore firstSem
+    \param Semaphor secondSem
+    \param int delay
+    
+*/
+/*! displays a message that is split in to 2 sections to show how a rendezvous works*/
+void taskTwo(std::shared_ptr<Semaphore> firstSem, std::shared_ptr<Semaphore> secondSem, int delay){
+  std::this_thread::sleep_for(std::chrono::seconds(delay));
+  std::cout <<"Task Two has arrived "<<std::endl;
+  //THIS IS THE RENDEZVOUS POINT!
+  secondSem->Signal();
+  firstSem->Wait();
+  std::cout << "Task Two has left "<<std::endl;
+}
+
+int main(void){
+  std::thread threadOne, threadTwo;
+  std::shared_ptr<Semaphore> sem1( new Semaphore);
+  std::shared_ptr<Semaphore> sem2( new Semaphore);
+  int taskOneDelay=5;
+  int taskTwoDelay=1;
+  /**< Launch the threads  */
+  threadOne=std::thread(taskTwo,sem1,sem2,taskTwoDelay);
+  threadTwo=std::thread(taskOne,sem1,sem2,taskOneDelay);
+  std::cout << "Launched from the main\n";
+  threadOne.join();
+  threadTwo.join();
+  return 0;
+}
